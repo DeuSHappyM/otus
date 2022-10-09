f6e7939880db3755c7a04f5d521cf40d492cc951
Belashev Mikhail <m.belashev@practus.ru>
2022-10-09 22:50:48 +0300

    Уравнение x^2-1 = 0 есть два корня кратности 1 (x1=1, x2=-1)

--- hw1/quadraticEquations.go
+++ hw1/quadraticEquations.go
@@ -1,5 +1,20 @@
 package hw1
 
+import (
+	"math"
+	"sort"
+)
+
+const float64EqualityThreshold = 1e-7
+
+func Compare(x, y float64) bool {
+	if math.Abs(x-y) > float64EqualityThreshold {
+		return false
+	} else {
+		return true
+	}
+}
+
 func Solve(a, b, c float64) []float64 {
 
 	D := b*b - 4*a*c
@@ -7,6 +22,10 @@ func Solve(a, b, c float64) []float64 {
 	switch {
 	case D < 0:
 		return []float64{}
+	case D > 0:
+		result := []float64{(-b + math.Sqrt(D)) / 2 * a, (-b - math.Sqrt(D)) / 2 * a}
+		sort.Float64s(result)
+		return result
 	default:
 		return []float64{}
 	}
--- hw1/quadraticEquations_test.go
+++ hw1/quadraticEquations_test.go
@@ -1,11 +1,27 @@
 package hw1
 
 import (
+	"sort"
 	"testing"
 )
 
-func TestSolveD0(t *testing.T) {
+func TestSolveDBelowZero(t *testing.T) {
 	if len(Solve(1, 0, 1)) != 0 {
 		t.Fatal("D = 0, no solution")
+	}
+}
+
+func TestSolveDAboveZero(t *testing.T) {
+
+	result := Solve(1, 0, -1)
+
+	if len(result) == 0 {
+		t.Fatal("D > 0, must be 2 solutions")
+	} else {
+		sort.Float64s(result)
+
+		if !(Compare(result[0], -1) && Compare(result[1], 1)) {
+			t.Fatalf("x1 must be -1, result[x1] = %f ,x2 must be 1, result[x2] = %f \n", result[0], result[1])
+		}
 	}
 }
